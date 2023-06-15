b4069c6d03f9f0cc447af3ed8650e0725ec4b3d2
m.raziei <mohammadraziei1375@gmail.com>
2023-06-16 02:55:03 +0430

    warning for if psl is not loaded

--- src/liburlparser/__init__.py
+++ src/liburlparser/__init__.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from ._core import __doc__, __version__, Url, Host, load_psl_from_path, load_psl_from_string, is_psl_loaded
 if not is_psl_loaded():
-    import warning
+    import warnings
     warnings.warn("PSL is not loaded! it may cause some perfemormace issues and return unexpected results!")
 
 
