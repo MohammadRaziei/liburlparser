2dc86f49439e94b4cf3eb40ad9b5c2b73cc94f81
m.raziei <mohammadraziei1375@gmail.com>
2023-06-27 15:39:35 +0430

    create __main__ file for using like "python -m liburlparser --version"

---
+++ src/liburlparser/__main__.py
@@ -0,0 +1,22 @@
+import sys
+from . import Url, Host, __version__, __doc__
+import argparse
+
+def show_if_not_none(_str, _class):
+    if _str is not None:
+        print(_class(_str).to_json())
+
+def main(args):
+    show_if_not_none(args.url, Url)
+    show_if_not_none(args.host, Host)
+    if args.version : print(__version__)
+    if args.doc : print(__doc__)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--url", type=str, default=None, help="enter entire url (for example: \"https://google.com/about\")")
+    parser.add_argument("--host", type=str, default=None, help="enter just host part of url (for example: \"google.com\")")
+    parser.add_argument('-v', '--version', action='store_true', help="showing version of module")
+    parser.add_argument('--doc', action='store_true', help="showing version of module")
+    args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
+    main(args)
