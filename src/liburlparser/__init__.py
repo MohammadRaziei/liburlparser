c9fe722565bc6ea1e34cb30cb804918cccab7fa9
m.raziei <mohammadraziei1375@gmail.com>
2023-07-03 10:54:04 +0430

    add update_psl

--- src/liburlparser/__init__.py
+++ src/liburlparser/__init__.py
@@ -1,14 +1,19 @@
 from __future__ import annotations
+import warnings
+import os
+import requests
 
 from ._core import __doc__, __version__, Url, Host, load_psl_from_path, load_psl_from_string, is_psl_loaded
 
-import warnings
 def warning_on_one_line(message, category, filename, lineno, file=None, line=None):
     return '%s:%s: %s: %s\n' % (filename, lineno, category.__name__, message)
 warnings.formatwarning = warning_on_one_line
 
+def psl_update():
+    resp = requests.get("https://publicsuffix.org/list/public_suffix_list.dat")
+    load_psl_from_string(resp.text)
+
 if not is_psl_loaded():
-    import os
     psl_filename = os.path.join(os.path.dirname(__file__), "public_suffix_list.dat")
     if os.path.exists(psl_filename):
         load_psl_from_path(psl_filename)
@@ -17,5 +22,5 @@ if not is_psl_loaded():
 
 __all__ = [
     "__doc__", "__version__", "Url", "Host", "load_psl_from_file",
-    "load_psl_from_string", "is_psl_loaded"
+    "load_psl_from_string", "is_psl_loaded", "psl_update"
 ]
