8e57f04f6a1798e6096a13395a719c585125559f
mohammadraziei <mohammadraziei1375@gmail.com>
2023-11-22 02:28:39 +0330

    rewrite test for host_data

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,37 +1,48 @@
-//
-// Created by mohammad on 5/22/23.
-//
+#include <gtest/gtest.h>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
 
 #include "urlparser.h"
-#include <gtest/gtest.h>
 
-TEST(MyTestSuite, MyTestCase) {
-  EXPECT_EQ("a.b.c.zarebin.ir",
-            TLD::Url("https://a.b.c.zarebin.ir/").fulldomain());
-  //    EXPECT_EQ("zarebin.ir", TLD::Url("zarebin.ir").fulldomain());
-  //    EXPECT_EQ("www.zarebin.ir", TLD::Url("www.zarebin.ir").fulldomain());
-  //    EXPECT_EQ("subdomain.zarebin.ir",
-  //    TLD::Url("subdomain.zarebin.ir").fulldomain());
-  EXPECT_EQ("subdomain.zarebin.ir",
-            TLD::Url("https://subdomain.zarebin.ir").fulldomain());
-  EXPECT_EQ("subdomain.zarebin.ir",
-            TLD::Url("https://subdomain.zarebin.ir/").fulldomain());
-  EXPECT_EQ("subdomain.zarebin.ir",
-            TLD::Url("https://subdomain.zarebin.ir/path").fulldomain());
-  EXPECT_EQ("subdomain.zarebin.ir",
-            TLD::Url("https://subdomain.zarebin.ir/path?q=1").fulldomain());
-  //    EXPECT_EQ("subdomain.zarebin.ir",
-  //    TLD::Url("subdomain.zarebin.ir/path?q=1").fulldomain());
-  //    EXPECT_EQ("zarebin.ir", TLD::Url("zarebin.ir/path?q=1").fulldomain());
-  //    EXPECT_EQ("zarebin.ir", TLD::Url("zarebin.ir:2020").fulldomain());
-  //    EXPECT_EQ("www.zarebin.ir",
-  //    TLD::Url("www.zarebin.ir:8888").fulldomain());
-  EXPECT_EQ(
-      "subdomain.zarebin.ir",
-      TLD::Url("https://subdomain.zarebin.ir:8080/path?q=1").fulldomain());
+struct HostData {
+    std::string url;
+    bool ignore_www;
+    std::string host;
+    std::string subdomain;
+    std::string domain;
+    std::string domain_name;
+    std::string suffix;
+};
+
+std::vector<HostData> load_host_data() {
+    std::vector<HostData> host_data_list;
+    std::ifstream f("data/host_data.csv");
+    std::string line;
+    while (std::getline(f, line)) {
+        std::istringstream ss(line);
+        HostData host_data;
+        ss >> host_data.url >> host_data.ignore_www >> host_data.host >>
+            host_data.subdomain >> host_data.domain >> host_data.domain_name >>
+            host_data.suffix;
+        host_data_list.push_back(host_data);
+    }
+    return host_data_list;
 }
 
-int main(int argc, char **argv) {
-  testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+TEST(HostTest, FromUrl) {
+    std::vector<HostData> host_data_list = load_host_data();
+    for (const auto& host_data : host_data_list) {
+        TLD::Host host = TLD::Host::fromUrl(host_data.url, host_data.ignore_www);
+//        ASSERT_EQ(std::string(host), host_data.host);
+        ASSERT_EQ(host.domain(), host_data.domain);
+        ASSERT_EQ(host.domainName(), host_data.domain_name);
+        ASSERT_EQ(host.suffix(), host_data.suffix);
+    }
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
