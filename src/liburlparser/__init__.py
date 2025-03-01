2420ceba65b3616c119f45eea0321fb50776a0ac
Mohammad Raziei <mohammadraziei1375@gmail.com>
2025-03-01 10:34:47 +0000

    update ruff

--- src/liburlparser/__init__.py
+++ src/liburlparser/__init__.py
@@ -4,5 +4,9 @@ from ._about import __version__
 from .core import Host, Url, __doc__, psl
 
 __all__ = [
-    "__doc__", "__version__", "Url", "Host", "psl"
+    "Host",
+    "Url",
+    "__doc__",
+    "__version__",
+    "psl"
 ]
